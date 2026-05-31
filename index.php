3eec7789f67f20adf5f2306be440b00ab673faea
AudreyOsei <olympioaudrey@gmail.com>
2026-05-31 23:04:45 +0100

    Landing page implemented

--- index.php
+++ index.php
@@ -1,9 +1,61 @@
 <?php
 
 include("includes/db.php");
-
-echo "Career Recommendation System Running Successfully!";
-echo "<br>";
-echo "Database Connected Successfully!";
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, intial-scale=1.0">
+    <title>Career Recommendation System</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    
+    <!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container">
+            <a class="navbar-brand fw-bold" href="#">Career Recommendation System</a>
+        </div>
+       </nav>
+
+    <!-- Hero Section -->
+    <section class="container py-5">
+
+        <div class="row align-items-center">
+
+            <div class="col-md-6">
+
+                <h1 class="fw-bold mb-3">
+                    Intelligent Career Recommendation System
+                </h1>
+
+                <p class="lead">
+                    Discover career paths that match your interests,
+                    skills, and preferences through personalized and
+                    explainable recommendations.
+                </p>
+
+                <a href="#" class="btn btn-primary btn-lg">
+                    Start Career Assessment
+                </a>
+
+            </div>
+
+            <div class="col-md-6 text-center">
+                <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+                     class="img-fluid"
+                     width="350">
+            </div>
+
+        </div>
+
+    </section>
+
+</body>
+</html>
